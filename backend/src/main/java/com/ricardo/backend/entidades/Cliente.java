6aeb5666e4689c97c74e80c7c0397e09fe2dc859
ricardowirz <ricardowirz@msn.com>
2021-01-26 23:26:59 -0300

    Adicionado a entidade cliente

--- backend/src/main/java/com/ricardo/backend/entidades/Cliente.java
+++ backend/src/main/java/com/ricardo/backend/entidades/Cliente.java
@@ -1,11 +1,115 @@
 package com.ricardo.backend.entidades;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Cliente implements Serializable{
 	private static final long serialVersionUID = -2460643265715148691L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String cpf;
+	private String nome;
+	private String email;
+	private Date dataNascimento;
+	private String sexo;
+	private String nomeSocial;
+	private String apelido;
+	private String telefone;
+	
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String cpf, String nome, String email, Date dataNascimento, String sexo,
+			String nomeSocial, String apelido, String telefone) {
+		this.id = id;
+		this.cpf = cpf;
+		this.nome = nome;
+		this.email = email;
+		this.dataNascimento = dataNascimento;
+		this.sexo = sexo;
+		this.nomeSocial = nomeSocial;
+		this.apelido = apelido;
+		this.telefone = telefone;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getNomeSocial() {
+		return nomeSocial;
+	}
+
+	public void setNomeSocial(String nomeSocial) {
+		this.nomeSocial = nomeSocial;
+	}
+
+	public String getApelido() {
+		return apelido;
+	}
+
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
 }
